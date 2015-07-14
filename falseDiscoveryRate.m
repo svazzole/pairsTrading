582b97be3d632d264bde9445bf5100deef0dbfa3
Simone Vazzoler <svazzole@gmail.com>
2015-07-14 12:04:55 +0200

    Added false discovery rate function

---
+++ falseDiscoveryRate.m
@@ -0,0 +1,28 @@
+function [fdr, logic, ix] = falseDiscoveryRate(pvalues, confidence)
+
+    [pOrd, ix] = sort(pvalues);
+    l = size(pOrd,1);
+    b = zeros(l,1);
+    
+    for i=1:l
+        
+        b(i) = (i/l)*confidence;
+        
+    end;
+    
+    t = (pOrd <= b);
+    k = find(t==1, 1, 'last');
+    
+    if k~=0
+    
+        t(1:k) = ones(k,1);
+    
+    end;
+    
+    fdr = ix.*t;
+    fdr(fdr==0) = [];
+    fdr = sort(fdr);
+    
+    logic = t(ix);
+    
+end
